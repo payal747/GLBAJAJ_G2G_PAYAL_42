30e89abae918f65b88e47e7882b5804ea3e12a2b
payal747 <chpayal747@gmail.com>
2026-02-09 12:00:14 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x,y,z;
+    printf("I am in G2G section of B.Tech-1st year\n");
+    printf("Enter the value of x and y-");
+    scanf("%d %d",&x,&y);
+    z=x+y;
+    printf("the sum of x and y is %d",z);
+    return 0;
+}
