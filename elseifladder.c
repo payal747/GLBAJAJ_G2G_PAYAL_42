9c9c16735f6b62746ad2b39597bed7656c67eefa
payal747 <chpayal747@gmail.com>
2026-02-16 11:38:01 +0530

    Add files via upload

---
+++ elseifladder.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter 3 numbers=");
+
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b&&a>c)
+    printf("a is greatest");
+    else if(b>a&&b>c)
+    printf("b is greatest");
+        else
+        printf("c is greatest");
+        return 0;
+
+}
